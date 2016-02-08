f161024410ee3111694f1041963d205d4f2e2ded
jaiz25 <jaizelle@gmail.com>
2016-02-08 05:01:31 +0000

    task3

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring goes here."""
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
