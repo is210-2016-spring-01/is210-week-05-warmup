c4a86129c3c05d173ed8805a85ae7e7a31d769c6
williamjcole <billy.cole@gmail.com>
2016-02-23 01:31:36 +0000

    tests

---
+++ tests/task_02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Testing the import of peanuts full namespace """
+
+
+import task_01.peanut
+
+TIME = task_01.peanut.BUTTER
---
+++ tests/task_03.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Importing module attribute """
+
+
+from task_01.peanut import BUTTER
+
+JELLY = BUTTER
