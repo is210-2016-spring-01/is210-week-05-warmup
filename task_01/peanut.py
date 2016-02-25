cef4435ee8288b324464e2bbed8af5a0d3550163
John Farrell <jf4rr3ll@gmail.com>
2016-02-25 15:07:10 +0000

    Week 5 Warm Up Tasks

---
+++ task_01/peanut.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A module to determine if the peanut butter is truthy or falsy"""
+
+BUTTER = True
+
+OIL = False
---
+++ task_02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A module to determine if the peanut butter is truthy or falsy"""
+
+import task_01.peanut
+
+TIME = task_01.peanut.BUTTER
