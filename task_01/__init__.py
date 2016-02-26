2998ac7f9b7f7ffa9891ccfb83ba2c883a19d161
Nicole Parrilla <nicole.parrilla@spsmail.cuny.edu>
2016-02-26 03:52:00 +0000

    Created Folder and files

---
+++ task_01/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Making a package and editing things in it"""
---
+++ task_01/peanut.py
@@ -0,0 +1,7 @@
+#!/use/bin/env python
+# -*- coding: utf-8 -*-
+""" Assigning truthy and falsy"""
+
+BUTTER = True
+
+OIL = False
