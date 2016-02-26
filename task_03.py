cd67e800254fdcbc610808a02b2aa00843024a0c
paisely65 <spscuny@gmail.com>
2016-02-26 10:58:47 +0000

    These taks are completes

--- task_03.py
+++ task_03.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A docstring that imports a module namespace"""
+"""A doctrsing for importing attributes"""
 
 import task_01.peanut
-
 print task_01.peanut.BUTTER
-TIME = task_01.peanut.BUTTER
+JELLY = 'I am a Truthy person'
+BUTTER = JELLY
