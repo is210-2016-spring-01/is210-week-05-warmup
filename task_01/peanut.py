e4354ba441c5860880d8e30c4ab31b3991a7ff64
Meriam Setti <mery81@hotmail.com>
2016-02-26 02:55:44 +0000

    Week-05-warmup

---
+++ task_01/peanut.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This file contain constants"""
+
+BUTTER = 1
+OIL = 0
---
+++ task_02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This file contain an import and a constant"""
+
+import task_01.peanut
+
+TIME = task_01.peanut.BUTTER
