73363c8c439c2d72177e6d6f3a2a9e4531bebae0
paisely65 <spscuny@gmail.com>
2016-02-26 03:44:54 +0000

    Task 01 has peanut and init

---
+++ task_01/peanut.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A really good example of using modules and packages"""
+
+
+BUTTER = 'I am a truthy person'
+OILY = 'I am a falsy person'
+
+print BUTTER
+print OILY
