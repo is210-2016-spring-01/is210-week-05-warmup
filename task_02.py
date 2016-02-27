ac22e894b52a6c910eaf89c5a0d34bbe9aa8e97e
ct3080a <coleen.thomas@spsmail.cuny.edu>
2016-02-27 03:39:55 +0000

    tasks 2 and 3

---
+++ task_02.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Impoting files"""
+
+import task_01.peanut
---
+++ task_03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Copying Module Attributes"""
+
+from peanut.py import BUTTER
+
+JELLY = 
